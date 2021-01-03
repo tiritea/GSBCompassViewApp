ece8aa22022d6973aa741a01c6892e842239e8a7
Dr. Gareth S. Bestor <xiphware@gmail.com>
2021-01-04 09:58:08 +1300

    Initial release

--- GSBCompassViewApp/ViewController.swift
+++ GSBCompassViewApp/ViewController.swift
@@ -6,14 +6,32 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    let compassView = GSBCompassView()
+    let locationManager = CLLocationManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        
+        locationManager.requestWhenInUseAuthorization()
+        if (CLLocationManager.headingAvailable()) {
+            view.addSubview(compassView)
+            compassView.frame = view.bounds
+            compassView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+            
+            // Start updating compass...
+            locationManager.delegate = self
+            locationManager.startUpdatingLocation()
+            locationManager.startUpdatingHeading()
+        }
     }
-
-
+    
+    public func locationManager(_ manager: CLLocationManager, didUpdateHeading heading: CLHeading) {
+        compassView.compassDegress = -heading.magneticHeading
+        compassView.setNeedsDisplay()
+    }
 }
 
